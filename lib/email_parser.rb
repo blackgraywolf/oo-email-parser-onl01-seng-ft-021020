e6d9496ca4d2140918da19eee7239b290c955724
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-19 20:23:29 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,7 +10,7 @@ class EmailParser
   def parse
     email_array = []
     email_array = @emails.split(/[,\s]/)
-   # email_array = email_array.reject { |e| e == ""}
+    email_array = email_array.reject { |e| e == ""}
     email_array.uniq
   end
 end
