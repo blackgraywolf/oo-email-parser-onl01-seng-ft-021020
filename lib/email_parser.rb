bd96197ce0cddf2e4e952113c016e309de05ffc4
industrious-generator-6081 <graywolf.niehaus@live.com>
2020-02-19 21:10:49 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -9,7 +9,8 @@ class EmailAddressParser
   attr_accessor :email
   def parse
     email_array = []
-    email_array = @emails.split(/[,\s]/)
+
+    email_array = @email.split(/[,\s]/)
     email_array = email_array.reject { |e| e == ""}
     email_array
   end
